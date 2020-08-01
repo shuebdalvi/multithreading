77b9de9c99c167dfaf03222ee7866a8177291649
Shueb Dalvi <shueb123@gmail.com>
2020-08-01 19:24:11 +0530

    minor changes for identifying threads

--- src/T01/startingthreads/L01ExtendingThread.java
+++ src/T01/startingthreads/L01ExtendingThread.java
@@ -2,39 +2,45 @@ package T01.startingthreads;
 
 public class L01ExtendingThread {
 
-	public static void main(String[] args) {
-		
-		Thread t1 = new RunMe();
-		// if t1.run() is called, then the method will execute in this same thread immediately
-		System.out.println("Going to start thread t1");		
-		t1.start();
-		System.out.println("Started the thread t1");
-		// starting it again gives illegelStateException
+    public static void main(String[] args) {
+
+        Thread t1 = new RunMe();
+        // if t1.run() is called, then the method will execute in this same thread immediately
+        System.out.println("Going to start thread t1");
+        t1.setName("ThreadName01");
+        t1.start();
+        System.out.println("Started the thread t1");
+        // starting it again gives illegelStateException
 //		t1.start();
-		Thread t2 = new RunMe();
-		System.out.println("GOing to start thread t2");		
-		t2.start();
-		System.out.println("Started the thread t2");
-		
-	} 
-	
+        try {
+            Thread.sleep(75);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Thread t2 = new RunMe();
+        System.out.println("Going to start thread t2");
+        t2.setName("ThreadName02");
+        t2.start();
+        System.out.println("Started the thread t2");
+
+    }
+
 }
 
-class RunMe extends Thread
-{
-	@Override
-	public void run() {
-		
-		super.run();
-		System.out.println("print long series");
-		for(int i=0;i<10;i++){
-			System.out.println("i now is "+i);
-			try {
-				Thread.sleep(200);
-			} catch (InterruptedException e) {
-		
-				e.printStackTrace();
-			}
-		}
-	}
+class RunMe extends Thread {
+    @Override
+    public void run() {
+
+        super.run();
+        System.out.println("print long series");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(String.format("For Thread %s , i now is %d", Thread.currentThread().getName(), i));
+            try {
+                Thread.sleep(25 * i);
+            } catch (InterruptedException e) {
+
+                e.printStackTrace();
+            }
+        }
+    }
 }
